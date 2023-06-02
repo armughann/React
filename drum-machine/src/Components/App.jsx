899977bb785e2179cebfbf481ab17ff0b942d702
Armughan <106377761+MAURXXX@users.noreply.github.com>
2023-06-02 16:44:29 +0500

    Drum Machine Project Added

---
+++ drum-machine/src/Components/App.jsx
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-12">
+                    <div id="drum-machine">
+                        <div className="display">
+                        </div>
+                        <div className="drum-pad" id="Q">
+                        </div>
+                        <div className="drum-pad" id="W">
+                        </div>
+                        <div className="drum-pad" id="E">
+                        </div>
+                        <div className="drum-pad" id="A">
+                        </div>
+                        <div className="drum-pad" id="S">
+                        </div>
+                        <div className="drum-pad" id="D">
+                        </div>
+                        <div className="drum-pad" id="Z">
+                        </div>
+                        <div className="drum-pad" id="X">
+                        </div>
+                        <div className="drum-pad" id="C">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
